f25d8217c637e159b1c75597d20e1d5ab4c6563e
Dan Raquel <draquel@webjynx.com>
2025-12-26 22:51:04 -0500

    refactored index scan for real tri indexing

--- Source/VoxelRDG/Private/MarchingCubes/MC_IndexPass.cpp
+++ Source/VoxelRDG/Private/MarchingCubes/MC_IndexPass.cpp
@@ -11,8 +11,10 @@ public:
 	SHADER_USE_PARAMETER_STRUCT(FMC_IndexScatterCS, FGlobalShader);
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
-		SHADER_PARAMETER(uint32, MaxIndices)
-		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, TotalVertsBuf)
+		SHADER_PARAMETER(uint32, NumCells)
+		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, TriCountPerCell)
+		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, TriOffsetPerCell)
+		SHADER_PARAMETER_RDG_BUFFER_SRV(StructuredBuffer<uint>, VertOffsetPerCell)
 		SHADER_PARAMETER_RDG_BUFFER_UAV(RWStructuredBuffer<uint>, OutIndices)
 	END_SHADER_PARAMETER_STRUCT()
 
@@ -23,30 +25,12 @@ IMPLEMENT_GLOBAL_SHADER(FMC_IndexScatterCS, "/Plugin/Voxel/MarchingCubes/MC_Inde
 
 FRDGBufferRef FMC_IndexPass::AddMC_IndexScatterPass(
 	FRDGBuilder& GraphBuilder,
-	FRDGBufferRef TotalVertsBuf,   // Scan.TotalVerts (structured uint[4])
+	FRDGBufferRef TriCountPerCell,
+	FRDGBufferRef TriOffsetPerCell,
+	FRDGBufferRef VertOffsetPerCell,
+	uint32 NumCells,
 	uint32 MaxIndices)
 {
-	// If a refactor accidentally passes null, RDG will crash in SetupPassResources.
-	// Make this pass robust by substituting a dummy 1-uint buffer.
-	if (!TotalVertsBuf)
-	{
-		FRDGBufferRef Dummy = GraphBuilder.CreateBuffer(
-			FRDGBufferDesc::CreateStructuredDesc(sizeof(uint32), 1),
-			TEXT("MC.TotalVerts.Dummy"));
-
-		AddClearUAVPass(GraphBuilder, GraphBuilder.CreateUAV(Dummy), 0);
-		TotalVertsBuf = Dummy;
-	}
-
-	if (MaxIndices == 0)
-	{
-		// Avoid creating weird 0-sized buffers / dispatch
-		FRDGBufferRef Empty = GraphBuilder.CreateBuffer(
-			FRDGBufferDesc::CreateStructuredDesc(sizeof(uint32), 1),
-			TEXT("MC.Indices.Empty"));
-		AddClearUAVPass(GraphBuilder, GraphBuilder.CreateUAV(Empty), 0);
-		return Empty;
-	}
 
 	FRDGBufferRef OutIndices = GraphBuilder.CreateBuffer(
 		FRDGBufferDesc::CreateStructuredDesc(sizeof(uint32), MaxIndices),
@@ -55,14 +39,16 @@ FRDGBufferRef FMC_IndexPass::AddMC_IndexScatterPass(
 	AddClearUAVPass(GraphBuilder, GraphBuilder.CreateUAV(OutIndices), 0);
 
 	auto* Params = GraphBuilder.AllocParameters<FMC_IndexScatterCS::FParameters>();
-	Params->MaxIndices    = MaxIndices;
-	Params->TotalVertsBuf = GraphBuilder.CreateSRV(TotalVertsBuf);
+	Params->NumCells      = NumCells;
+	Params->TriCountPerCell = GraphBuilder.CreateSRV(TriCountPerCell);
+	Params->TriOffsetPerCell = GraphBuilder.CreateSRV(TriOffsetPerCell);
+	Params->VertOffsetPerCell = GraphBuilder.CreateSRV(VertOffsetPerCell);
 	Params->OutIndices    = GraphBuilder.CreateUAV(OutIndices);
 
 	TShaderMapRef<FMC_IndexScatterCS> CS(GetGlobalShaderMap(GMaxRHIFeatureLevel));
 
-	const uint32 Threads = 256;
-	const uint32 GroupsX = (MaxIndices + Threads - 1) / Threads;
+	const uint32 ThreadsPerGroup = 64; // must match [numthreads(64,1,1)]
+	const uint32 GroupsX = (NumCells + ThreadsPerGroup - 1) / ThreadsPerGroup;
 
 	FComputeShaderUtils::AddPass(
 		GraphBuilder,
--- Source/VoxelRDG/Private/MarchingCubes/MC_IndexPass.h
+++ Source/VoxelRDG/Private/MarchingCubes/MC_IndexPass.h
@@ -4,6 +4,7 @@
 class VOXELRDG_API FMC_IndexPass
 {
 public:
-	static FRDGBufferRef AddMC_IndexScatterPass(FRDGBuilder& GraphBuilder, FRDGBufferRef TotalVertsBuf, uint32
-	                                                        MaxIndices);
+	static FRDGBufferRef AddMC_IndexScatterPass(FRDGBuilder& GraphBuilder, FRDGBufferRef TriCountPerCell, FRDGBufferRef TriOffsetPerCell, FRDGBufferRef
+	                                            VertOffsetPerCell, uint32 NumCells, uint32
+	                                            MaxIndices);
 };
