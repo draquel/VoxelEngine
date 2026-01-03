001e8ec55d11d9cec517af40f934a50f03d5d290
Dan Raquel <draquel@webjynx.com>
2026-01-03 05:16:33 -0500

    Refactored `FChunkMeshRenderData::InitFromPooled` to simplify initialization logic, replaced redundant comments, and improved variable consistency with pooled buffer support.

--- Source/VoxelRender/Private/VoxelChunkMeshRenderData.cpp
+++ Source/VoxelRender/Private/VoxelChunkMeshRenderData.cpp
@@ -5,33 +5,24 @@
 namespace VoxelRender
 {
 	void FChunkMeshRenderData::InitFromPooled(
-		const TRefCountPtr<FRDGPooledBuffer>& Pos,
-		const TRefCountPtr<FRDGPooledBuffer>& Nor,
-		const TRefCountPtr<FRDGPooledBuffer>& Ind,
-		uint32 InNumVerts,
-		uint32 InNumIndices)
+	const TRefCountPtr<FRDGPooledBuffer>& Pos,
+	const TRefCountPtr<FRDGPooledBuffer>& Nor,
+	const TRefCountPtr<FRDGPooledBuffer>& Ind,
+	uint32 InNumVerts,
+	uint32 InNumIndices)
 	{
 		ResetRHI();
 
-		VertexCount   = InNumVerts;
-		IndexCount = InNumIndices;
+		VertexPooled  = Pos;
+		NormalsPooled = Nor;
+		IndexPooled   = Ind;
 
-		// FRDGPooledBuffer::GetRHI() returns FBufferRHIRef
+		VertexCount = InNumVerts;
+		IndexCount  = InNumIndices;
+
 		if (Pos) PositionBufferRHI = Pos->GetRHI();
 		if (Nor) NormalBufferRHI   = Nor->GetRHI();
 		if (Ind) IndexBufferRHI    = Ind->GetRHI();
-
-		// SRVs are optional (only if you need them in shaders)
-		// NOTE: For structured buffers, you usually create SRV with format PF_Unknown
-		// because it is a structured buffer view. That’s OK.
-		// if (PositionBufferRHI.IsValid())
-		// {
-		// 	PositionSRV = RHICreateShaderResourceView(PositionBufferRHI);
-		// }
-		// if (NormalBufferRHI.IsValid())
-		// {
-		// 	NormalSRV = RHICreateShaderResourceView(NormalBufferRHI);
-		// }
 	}
 
 	void FChunkMeshRenderData::ReleaseRHI()
