e69c90d7223ade178c6be3c6bc43f0c3a7a1b6a1
Dan Raquel <draquel@webjynx.com>
2026-01-03 05:16:47 -0500

    Refactored `FExternalVertexBuffer` and `FExternalIndexBuffer` to support typed SRV creation, added safer buffer initialization logic, and improved compatibility with UE 5.7 rendering workflows.

--- Source/VoxelRDG/Public/VoxelExternalVertexBuffer.h
+++ Source/VoxelRDG/Public/VoxelExternalVertexBuffer.h
@@ -1,49 +1,74 @@
-﻿// VoxelExternalVertexBuffer.h
-
-#pragma once
-#include "RenderResource.h"
-#include "RHI.h"
-
-/**
+﻿/**
  * Minimal wrapper that lets a raw GPU buffer be used as a VertexBuffer
  * so FVertexStreamComponent can reference it (UE5.7 requirement).
  */
-class FExternalVertexBuffer final : public FVertexBuffer
-{
-public:
-	void SetRHI(const FBufferRHIRef& InRHI)
+
+#pragma once
+
+#include "RenderResource.h"
+#include "RHI.h"
+#include "RHICommandList.h"
+
+
+	// Typed external VB wrapper that also provides an SRV (required by LocalVertexFactory UB creation).
+	class FExternalVertexBuffer : public FVertexBufferWithSRV
 	{
-		VertexBufferRHI = InRHI;
-	}
+	public:
+		void SetSource(const FBufferRHIRef& InBuffer, uint32 InStride, uint32 InNumElements, EPixelFormat InFormat)
+		{
+			SourceBufferRHI = InBuffer;
+			Stride          = InStride;
+			NumElements     = InNumElements;
+			Format          = InFormat;
+		}
 
-	virtual void InitRHI(FRHICommandListBase&) override
+		virtual void InitRHI(FRHICommandListBase& RHICmdList) override
+		{
+			check(SourceBufferRHI.IsValid());
+			check(Format != PF_Unknown);
+
+			VertexBufferRHI = SourceBufferRHI;
+
+			// Typed SRV is REQUIRED if the VF uniform buffer expects a resource SRV.
+			// UE 5.7 has this overload:
+			ShaderResourceViewRHI = RHICmdList.CreateShaderResourceView(VertexBufferRHI, Stride, Format);
+			check(ShaderResourceViewRHI.IsValid());
+		}
+
+		virtual void ReleaseRHI() override
+		{
+			FVertexBufferWithSRV::ReleaseRHI();
+			SourceBufferRHI.SafeRelease();
+		}
+
+	private:
+		FBufferRHIRef  SourceBufferRHI;
+		uint32         Stride      = 0;
+		uint32         NumElements = 0;
+		EPixelFormat   Format      = PF_Unknown;
+	};
+
+
+	// Same idea for indices
+	class FExternalIndexBuffer final : public FIndexBuffer
 	{
-		// NO-OP: RHI already assigned
-	}
-};
+	public:
+		void SetSource(const FBufferRHIRef& InBuffer)
+		{
+			Source = InBuffer;
+		}
 
+		virtual void InitRHI(FRHICommandListBase& RHICmdList) override
+		{
+			IndexBufferRHI = Source;
+		}
 
-// Same idea for indices
-class FExternalIndexBuffer final : public FIndexBuffer
-{
-public:
-	void SetRHI(const FBufferRHIRef& InBuffer)
-	{
-		Source = InBuffer;
-	}
+		virtual void ReleaseRHI() override
+		{
+			IndexBufferRHI.SafeRelease();
+			Source.SafeRelease();
+		}
 
-	virtual void InitRHI(FRHICommandListBase& RHICmdList) override
-	{
-		IndexBufferRHI = Source;
-	}
-
-	virtual void ReleaseRHI() override
-	{
-		IndexBufferRHI.SafeRelease();
-		Source.SafeRelease();
-	}
-
-private:
-	FBufferRHIRef Source;
-};
-
+	private:
+		FBufferRHIRef Source;
+	};
