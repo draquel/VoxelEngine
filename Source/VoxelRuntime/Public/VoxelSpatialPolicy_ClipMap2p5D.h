68bd3622bc9e3272c27cf34c6eaf1b5913d7c9b2
Dan Raquel <draquel@webjynx.com>
2026-01-06 00:57:26 -0500

    Added `ClipMap2p5D` spatial policy: introduced `FVoxelSpatialPolicy_ClipMap2p5D` for camera-driven 2.5D LOD demand computation and robust chunk prioritization logic.

---
+++ Source/VoxelRuntime/Public/VoxelSpatialPolicy_ClipMap2p5D.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "IVoxelSpacialPolicy.h"
+
+namespace VoxelRuntime
+{
+	class VOXELRUNTIME_API FVoxelSpatialPolicy_ClipMap2p5D final : public Voxel::IVoxelSpatialPolicy
+	{
+	
+	public:
+		FVoxelSpatialPolicy_ClipMap2p5D() = default;
+		virtual ~FVoxelSpatialPolicy_ClipMap2p5D() override = default;
+		
+		virtual void ComputeDemands(
+			const FVoxelWorldSettings& World, 
+			const FVoxelLODPolicyParams& Params,
+			const TArray<FVector>& CameraPositionsWS,
+			TArray<FVoxelChunkDemand>& OutDemands) override;	
+	};
+}
