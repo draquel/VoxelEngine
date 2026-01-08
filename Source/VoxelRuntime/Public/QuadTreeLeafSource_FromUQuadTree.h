069995ca7a8a530af0d1af4bd92b192ff68f54d4
Dan Raquel <draquel@webjynx.com>
2026-01-08 12:56:23 -0500

    added guard tiles to avoid far-field collapse in chunk eviction.

--- Source/VoxelRuntime/Public/QuadTreeLeafSource_FromUQuadTree.h
+++ Source/VoxelRuntime/Public/QuadTreeLeafSource_FromUQuadTree.h
@@ -46,7 +46,8 @@ namespace VoxelRuntime
 
 			const float BaseTile = World.SurfaceSettings.BaseTileSizeWS;
 			const int32 ExtTiles     = FMath::Max(1, Params.SurfaceExtentTiles0);
-			const int32 TilesPerSide = 2 * ExtTiles + 1;
+			const int32 GuardTiles = 1;
+			const int32 TilesPerSide = 2 * (ExtTiles + GuardTiles) + 1;
 			const float SizeWS       = float(TilesPerSide) * BaseTile;
 			const float RadiusWS     = SizeWS * 0.5f;
 
