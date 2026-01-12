3ca3f7d380dbe6733b524f417953af90e2d45ac2
Dan Raquel <draquel@webjynx.com>
2026-01-11 22:05:54 -0500

    Added `OverhangSurfaceBand` and `CaveWarpAmp` parameters to `FVoxelNoiseParams` and enhanced `Density_OverhangPerturb` and `Density_CavesCarve` with surface masking and domain warping logic for improved terrain detail and cave features.

--- Source/VoxelCore/Public/VoxelNoiseParams.h
+++ Source/VoxelCore/Public/VoxelNoiseParams.h
@@ -30,12 +30,18 @@ struct VOXELCORE_API FVoxelNoiseParamsCPU
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise", meta=(ClampMin="1", ClampMax="8"))
 	int32 OverhangOctaves = 4;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise")
+	float OverhangSurfaceBand = 30.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise")
 	float CaveFreq = 3.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise")
 	float CaveAmp = 40.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise")
+	float CaveWarpAmp = 10.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Voxel|Noise")
 	float CaveThreshold = 0.35f;
@@ -58,9 +64,11 @@ BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FVoxelNoiseParams, VOXELCORE_API)
 	SHADER_PARAMETER(float,    OverhangFreq)
 	SHADER_PARAMETER(float,    OverhangAmp)
 	SHADER_PARAMETER(uint32,   OverhangOctaves)
+	SHADER_PARAMETER(float,    OverhangSurfaceBand)
 
 	SHADER_PARAMETER(float,    CaveFreq)
 	SHADER_PARAMETER(float,    CaveAmp)
+	SHADER_PARAMETER(float,    CaveWarpAmp)
 	SHADER_PARAMETER(float,    CaveThreshold)
 	SHADER_PARAMETER(float,    CaveSoftness)
 
@@ -81,9 +89,11 @@ inline FVoxelNoiseParams MakeVoxelNoiseParams(const FVoxelNoiseParamsCPU& In)
 	Out.OverhangFreq   = In.OverhangFreq;
 	Out.OverhangAmp    = In.OverhangAmp;
 	Out.OverhangOctaves= In.OverhangOctaves;
+	Out.OverhangSurfaceBand = In.OverhangSurfaceBand;
 
 	Out.CaveFreq       = In.CaveFreq;
 	Out.CaveAmp        = In.CaveAmp;
+	Out.CaveWarpAmp    = In.CaveWarpAmp;
 	Out.CaveThreshold  = In.CaveThreshold;
 	Out.CaveSoftness   = In.CaveSoftness;
 
