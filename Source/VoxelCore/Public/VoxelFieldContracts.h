0d54a1b084af6a39b526d0de34a443ff52a2d6f6
Dan Raquel <draquel@webjynx.com>
2026-01-23 10:01:00 -0500

    Added `FVoxelChunkFieldLayout`, `FVoxelChunkFieldMetadata`, and `FVoxelChunkFieldOutputs` structs to define field data, metadata, and outputs for voxel chunks, including a utility for sample index computation.

---
+++ Source/VoxelCore/Public/VoxelFieldContracts.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+namespace Voxel::Contracts
+{
+	struct FVoxelChunkFieldLayout
+	{
+		uint32 CellsPerAxis   = 0;
+		uint32 SamplesPerAxis = 0;
+		uint32 NumSamples     = 0;
+	};
+
+	struct FVoxelChunkFieldMetadata
+	{
+		FVector3f ChunkOriginWS = FVector3f::ZeroVector;
+		float StepSizeWS = 0.f;
+		uint32 Seed = 0;
+		float IsoLevel = 0.f;
+	};
+
+	struct FVoxelChunkFieldOutputs
+	{
+		FVoxelChunkFieldLayout Layout;
+		FVoxelChunkFieldMetadata Meta;
+
+		uintptr_t DensitySRV = 0;
+		uintptr_t DensityUAV = 0;
+		uintptr_t MaterialSRV = 0;
+		uintptr_t MaterialUAV = 0;
+	};
+
+	FORCEINLINE uint32 ComputeFieldSampleIndex(uint32 X, uint32 Y, uint32 Z, uint32 SamplesPerAxis)
+	{
+		return X + Y * SamplesPerAxis + Z * SamplesPerAxis * SamplesPerAxis;
+	}
+}
